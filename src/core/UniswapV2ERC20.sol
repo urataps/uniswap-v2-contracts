421a5475e237d4521f8a195cf1697dc2c451fb02
urataps <alexeluca.spataru@gmail.com>
2024-03-05 16:41:20 +0200

    chore: add uniswap v2

---
+++ src/core/UniswapV2ERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.24;
+
+import { ERC20 } from "solady/src/tokens/ERC20.sol";
+
+contract UniswapV2ERC20 is ERC20 {
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    function name() public pure override returns (string memory) {
+        return "Uniswap V2";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "UNI-V2";
+    }
+}
---
+++ src/core/UniswapV2Factory.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.24;
+
+import { IUniswapV2Factory } from "./interfaces/IUniswapV2Factory.sol";
+import { UniswapV2Pair, IUniswapV2Pair } from "./UniswapV2Pair.sol";
+
+contract UniswapV2Factory is IUniswapV2Factory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    constructor(address _feeToSetter) {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, "IDENTICAL_ADDRESSES");
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "ZERO_ADDRESS");
+        require(getPair[token0][token1] == address(0), "PAIR_EXISTS"); // single check is sufficient
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        pair = address(new UniswapV2Pair{salt: salt}());
+        IUniswapV2Pair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, "FORBIDDEN");
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, "FORBIDDEN");
+        feeToSetter = _feeToSetter;
+    }
+}
